a34abb011938ba78a6209ccd6cfad43750b99458
TecnoDesign <marcos21.009@gmail.com>
2022-03-22 23:36:04 -0500

    Update for EncoreCMS 2

--- Repositories/Eloquent/EloquentTagRepository.php
+++ Repositories/Eloquent/EloquentTagRepository.php
@@ -2,6 +2,9 @@
 
 namespace Modules\Tag\Repositories\Eloquent;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
 use Modules\Tag\Events\TagIsCreating;
 use Modules\Tag\Events\TagIsUpdating;
@@ -14,14 +17,18 @@ class EloquentTagRepository extends EloquentBaseRepository implements TagReposit
     /**
      * Get all the tags in the given namespace
      * @param string $namespace
-     * @return \Illuminate\Database\Eloquent\Collection
+     * @return Collection
      */
-    public function allForNamespace($namespace)
+    public function allForNamespace($namespace): Collection
     {
         return $this->model->with('translations')->where('namespace', $namespace)->get();
     }
-
-    public function create($data)
+    /**
+     * Create a resource
+     * @param  $data
+     * @return Model|Collection|Builder|array|null
+     */
+    public function create($data): Model|Collection|Builder|array|null
     {
         event($event = new TagIsCreating($data));
         $tag = $this->model->create($event->getAttributes());
@@ -30,8 +37,13 @@ class EloquentTagRepository extends EloquentBaseRepository implements TagReposit
 
         return $tag;
     }
-
-    public function update($tag, $data)
+    /**
+     * Update a resource
+     * @param  $tag
+     * @param array $data
+     * @return Model|Collection|Builder|array|null
+     */
+    public function update($tag, $data): Model|Collection|Builder|array|null
     {
         event($event = new TagIsUpdating($tag, $data));
         $tag->update($event->getAttributes());
